c25dd9ccec6fc5acdaf390221dfe6d58f0fc036c
Odin Beuchat <66629792+dindin-nibnib@users.noreply.github.com>
2022-06-11 21:43:58 +0200

    Added dungeon command

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,12 +4,26 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
-    
-]
-    .map(command => command.toJSON());
+    new SlashCommandBuilder()
+    .setName("dungeon")
+    .setDescription("Sends someone to the dungeon")
+    .addUserOption(option =>
+        option.setName("minion")
+        .setDescription("The minion to send to the dungeon")
+        .setRequired(true)),
 
+    new SlashCommandBuilder()
+    .setName("undungeon")
+    .setDescription("Gets someone out of the dungeon")
+    .addUserOption(option =>
+        option.setName("minion")
+        .setDescription("The minion to get out of the dungeon")
+        .setRequired(true))
+]
+.map(command => command.toJSON());
+    
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error);
+.then(() => console.log('Successfully registered application commands.'))
+.catch(console.error);
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import "reflect-metadata";
 import { Client } from "discordx";
-import { Intents, Message } from "discord.js";
+import { GuildMember, Intents, Message } from "discord.js";
 import { randomInt } from "crypto";
-const { clientId, guildId, token } = require("./config.json");
+import { APIInteractionDataResolvedGuildMember, Snowflake } from "discord-api-types/v9";
+const { clientId, guildId, token, dungeonRole } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({
@@ -17,11 +18,78 @@ client.once("ready", () => {
     console.log("Ready!")
 });
 
+client.on("interactionCreate", interaction => {
+    if (!interaction.isCommand())
+        return;
+
+    let minionSnowflake = interaction.options.get("minion")?.value;
+    if (minionSnowflake === undefined)
+        return;
+
+    let minion = client.guilds.resolve(guildId).members.resolve(minionSnowflake.toString())
+
+
+    switch (interaction.commandName) {
+        case "dungeon":
+            if (minion === null)
+                return;
+            if (minion.roles.cache.has(dungeonRole)) {
+                interaction.reply({
+                    content: minion.displayName + " already is in the dungeon, use /undungeon <member> to get them out!",
+                    ephemeral: true
+                });
+            } else {
+                minion.roles.add(dungeonRole).then((minion) => {
+                    interaction.reply({
+                        content: "Sent " + minion.displayName + " to the dungeon! (bad minion!)",
+                        ephemeral: true
+                    });
+                })
+                    .catch((err) => {
+                        interaction.reply({
+                            content: "Couldn't get " + minion?.displayName + " to the dungeon, they really wanted to stay in it... \n\n" + err,
+                            ephemeral: true
+                        });
+                    });
+
+            }
+            break;
+
+        case "undungeon":
+            if (minion === null)
+                return;
+
+            if (!minion.roles.cache.has(dungeonRole)) {
+                interaction.reply({
+                    content: minion.displayName + " Isn't in the dungeon, use /dungeon to send them in it!",
+                    ephemeral: true
+                });
+            } else {
+                minion.roles.remove(dungeonRole).then((minion) => {
+                    interaction.reply({
+                        content: "Got " + minion.displayName + " out of the dungeon! (bad minion!)",
+                        ephemeral: true
+                    });
+                })
+                    .catch((err) => {
+                        interaction.reply({
+                            content: "Couldn't get " + minion?.displayName + " out of the dungeon, they were too strong... \n\n" + err,
+                            ephemeral: true
+                        });
+                    });
+
+            }
+            break;
+
+        default:
+            break;
+    }
+})
+
 client.on("messageCreate", (message) => {
     // To avoid bot loop (bot answering to itself)
-    if (message.author.bot) {
+    if (message.author.bot)
         return;
-    }
 
     if (message.channel.type == "GUILD_TEXT") {
         if (message.channel.nsfw) {
